74a70e3e9f1601e9395a40604b9352d3a27a8a9a
Daniel070999 <danielixo99@gmail.com>
2024-02-01 20:00:25 -0500

    integracion de front con back para lecturas de validacion en URL e IP

--- Front/src/pages/api/ApiC.jsx
+++ Front/src/pages/api/ApiC.jsx
@@ -1,20 +1,69 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function ApiC() {
-
     const validarURL = useRef(null);
     const validarIP = useRef(null);
     const validarArchivo = useRef(null);
+    const [data, setData] = useState();
 
-    const validateURL = () => {
-        console.log(validarURL.current.value);
-    }
-    const validateIP = () => {
-        console.log(validarIP.current.value);
-    }
+    const makeRequest = async (url, body) => {
+        try {
+            const myHeaders = new Headers({
+                "Content-Type": "application/json",
+            });
+
+            const requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: JSON.stringify(body),
+                redirect: 'follow',
+            };
+
+            const response = await fetch(url, requestOptions);
+            const result = await response.text();
+            return result;
+        } catch (error) {
+            console.log('error', error);
+        }
+    };
+
+    const closeModal = () => {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    };
+
+    const validateURL = async () => {
+        const urlForm = validarURL.current.value;
+        if (!urlForm) {
+            alert('Debe ingresar una URL');
+            return;
+        }
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+        setData();
+        setData(await makeRequest("http://localhost:8080/scan/url", { url: urlForm }));
+    };
+
+    const validateIP = async () => {
+        const IPForm = validarIP.current.value;
+        if (!IPForm) {
+            alert('Debe ingresar una IP');
+            return;
+        }
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+        setData();
+        setData(await makeRequest("http://localhost:8080/scan/ip", { ip: IPForm }));
+    };
+
     const validateFile = () => {
-        console.log(validarArchivo.current.files[0]);
-    }
+        const fileForm = validarArchivo.current.files[0];
+        if (!fileForm) {
+            alert('Debe subir un archivo');
+            return;
+        }
+        console.log(fileForm);
+    };
 
     return (
         <>
@@ -26,10 +75,10 @@ function ApiC() {
                         type="text"
                         name="text"
                         id="textUrl"
-                        placeholder="Ingrese una URL"
+                        placeholder="https://www.google.com"
                         className="input-field"
                     />
-                    <button onClick={() => validateURL()} className="button-30">Validar</button>
+                    <button onClick={validateURL} className="button-30">Validar</button>
                 </div>
             </div>
             <div className="form-group">
@@ -40,10 +89,10 @@ function ApiC() {
                         type="text"
                         name="text"
                         id="textIP"
-                        placeholder="Ingrese una IP"
+                        placeholder="8.8.8.8"
                         className="input-field"
                     />
-                    <button onClick={() => validateIP()} className="button-30">Validar</button>
+                    <button onClick={validateIP} className="button-30">Validar</button>
                 </div>
             </div>
             <div className="form-group">
@@ -56,10 +105,17 @@ function ApiC() {
                         id="file"
                         className="input-field"
                     />
-                    <button onClick={() => validateFile()} className="button-30">Validar</button>
+                    <button onClick={validateFile} className="button-30">Validar</button>
+                </div>
+            </div>
+            <div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span className="close" onClick={closeModal}>&times;</span>
+                    <p>{!data ? (<>Cargando...</>) : (<>{data}</>)}</p>
                 </div>
             </div>
         </>
-    )
+    );
 }
+
 export default ApiC;
